541c1a5cce730938197aaf37f6ebdce8f4162886
Polya04 <mykhalevychpolina1@gmail.com>
2023-05-14 19:20:24 +0300

    Creating abstract class Theme and implementing real themes based on Theme class

--- tests.py
+++ tests.py
@@ -1,5 +1,9 @@
+from abc import ABC, abstractmethod
+
+
 class VirtualAssistant:
     _instance = None
+    _THEMES = ["Фізика", "Математика", "Географія", "Філологія", "Робота з текстом", "Загальні", 'Гра "історія"']
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
@@ -8,3 +12,55 @@ class VirtualAssistant:
 
     def __init__(self):
         self._dialogues = []
+
+    def answer(self):
+        print(
+            f"Вітаю, мене звати {VirtualAssistant.__name__}. Ви можете задати мені питання з наступних тем: математика, фізика, філологія, географія.")
+
+
+class Theme(ABC):
+    @abstractmethod
+    def name(self):
+        pass
+
+
+class Math(Theme):
+    @property
+    def name(self):
+        return "Математика"
+
+
+class Physics(Theme):
+    @property
+    def name(self):
+        return "Фізика"
+
+
+class Geography(Theme):
+    @property
+    def name(self):
+        return "Географія"
+
+
+class Philology(Theme):
+    @property
+    def name(self):
+        return "Філологія"
+
+
+class General(Theme):
+    @property
+    def name(self):
+        return "Загальні"
+
+
+class Text(Theme):
+    @property
+    def name(self):
+        return "Робота з текстом"
+
+
+class Game(Theme):
+    @property
+    def name(self):
+        return 'Гра "історія"'
