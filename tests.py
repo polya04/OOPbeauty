fc77fee2dc558e0f10f05eb74ccd5a53b1c8ba65
Polya04 <mykhalevychpolina1@gmail.com>
2023-05-14 21:01:50 +0300

    Strategy design pattern implementation for themes

--- tests.py
+++ tests.py
@@ -1,27 +1,21 @@
 from abc import ABC, abstractmethod
-
-
-class VirtualAssistant:
-    _instance = None
-    _THEMES = ["Фізика", "Математика", "Географія", "Філологія", "Робота з текстом", "Загальні", 'Гра "історія"']
-
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
-
-    def __init__(self):
-        self._dialogues = []
-
-    def answer(self):
-        print(
-            f"Вітаю, мене звати {VirtualAssistant.__name__}. Ви можете задати мені питання з наступних тем: математика, фізика, філологія, географія.")
+import math
 
 
 class Theme(ABC):
+    _themes = []
+
     @abstractmethod
     def name(self):
         pass
+
+    def action(self):
+        return f"Ви обрали тему {self.name}. Ви можете задати  мені питання з наступних" \
+               f" тем: {', '.join(self._themes[:-1]) + self._themes[-1]}. "
+
+    @property
+    def themes(self):
+        return self._themes
 
 
 class Math(Theme):
@@ -31,6 +25,8 @@ class Math(Theme):
 
 
 class Physics(Theme):
+    _themes = ["Вивести гравітаційну сталу"]
+
     @property
     def name(self):
         return "Фізика"
@@ -64,3 +60,32 @@ class Game(Theme):
     @property
     def name(self):
         return 'Гра "історія"'
+
+
+class G(Physics):
+    def action(self):
+        return "G is approximately 6.67430 × 10-11 N(m/kg)^2"
+
+
+class Coulomb(Physics):
+    def action(self):
+        return f"{str(8.9875517923e+9)} N*m^2/C^2"
+
+
+class VirtualAssistant:
+    _instance = None
+    _dict = {Physics(): [G, Coulomb]}
+    _THEMES = [Math(), Physics(), Geography(), Philology(), General(), Text(), Game()]
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self._dialogues = []
+
+    def answer(self):
+        print(
+            f"Вітаю, мене звати {VirtualAssistant.__name__}. Ви можете задати мені питання з наступних тем: "
+            f"{', '.join([theme.name for theme in self._THEMES[:-1]])}, {self._THEMES[-1].name}.")
